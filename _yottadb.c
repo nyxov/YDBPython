e9adb1e5dd71f8113b4381d9dba1a11a709fa2de
Peter Goss <gossrock@gmail.com>
2020-09-09 15:54:25 +0000

    cleaned up  a bit more.

--- _yottadb.c
+++ _yottadb.c
@@ -305,26 +305,26 @@ static int validate_py_keys_sequence_bytes(PyObject *keys_sequence,
         ret = YDBPY_INVALID_KEY_IN_SEQUENCE_NOT_LIST_OR_TUPLE;
       }
       /* validate item/key length [1 or 2] */
-      if ((YDBPY_VALID == ret) && !((1 == len_key_seq) || (2 == len_key_seq))) {
+      else if (!((1 == len_key_seq) || (2 == len_key_seq))) {
         snprintf(error_message, YDBPY_MAX_REASON,
                  YDBPY_ERRMSG_KEY_IN_SEQUENCE_INCORECT_LENGTH, i);
         ret = YDBPY_INVALID_KEY_IN_SEQUENCE_INCORECT_LENGTH;
       }
       /* validate item/key first element (varname) type */
-      if ((YDBPY_VALID == ret) && !PyBytes_Check(varname)) {
+      else if (!PyBytes_Check(varname)) {
         snprintf(error_message, YDBPY_MAX_REASON,
                  YDBPY_ERRMSG_KEY_IN_SEQUENCE_VARNAME_NOT_BYTES, i);
         ret = YDBPY_INVALID_KEY_IN_SEQUENCE_VARNAME_NOT_BYTES;
       }
       /* validate item/key first element (varname) length */
-      if ((YDBPY_VALID == ret) && (YDB_MAX_IDENT < len_varname)) {
+      else if ((YDB_MAX_IDENT < len_varname)) {
         snprintf(error_message, YDBPY_MAX_REASON,
                  YDBPY_ERRMSG_KEY_IN_SEQUENCE_VARNAME_TOO_LONG, i, len_varname,
                  YDB_MAX_IDENT);
         ret = YDBPY_INVALID_KEY_IN_SEQUENCE_VARNAME_TOO_LONG;
       }
       /* validate item/key second element (subsarray) if it exists */
-      if ((YDBPY_VALID == ret) && (2 == len_key_seq)) {
+      else if ((2 == len_key_seq)) {
         if (Py_None != subsarray) {
           ret = validate_sequence_of_bytes(subsarray, YDB_MAX_SUBS, YDB_MAX_STR,
                                            error_sub_reason);
