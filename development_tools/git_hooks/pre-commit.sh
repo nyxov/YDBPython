4183595d99aa2112778fdc8c507cb166fbaac431
Peter Goss <gossrock@gmail.com>
2020-09-29 14:35:20 +0000

    modified pre-commit hook to automatically format .c and .h files when file is then same as what has been added.

--- development_tools/git_hooks/pre-commit.sh
+++ development_tools/git_hooks/pre-commit.sh
@@ -5,8 +5,14 @@ for file in `git diff-index --cached --name-only HEAD | grep -iE '\.(c|h)$' ` ; 
       echo "${file}"
       out=$(git clang-format -v --diff "${file}")
       if [ "$out" != *"no modified files to format"* ] && [ "$out" != *"clang-format did not modify any files"* ]; then
-        echo "change to '${file}' that is being committed is formatted incorrectly"
-        echo "run 'clang-format -i ${file}' to fix and re-add."
+        if [ "$(git diff ${file})" == "" ]; then
+          echo "Automatically fixing formatting errors in ${file} with \"clang-format\"."
+          clang-format -i ${file}
+          git add ${file}
+        else
+          echo "File to be committed \"${file}\" has formatting errors that can be fixed automatically;"
+          echo "however, there have been changes since it was added, so we will abort commit to avoid conflicts."
         exit 1;
+        fi
       fi
 done
