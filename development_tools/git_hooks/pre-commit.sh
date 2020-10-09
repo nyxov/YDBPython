61a9f3a90049227174cba06cff898ade58e893fe
Peter Goss <gossrock@gmail.com>
2020-10-09 12:44:36 +0000

    Added black formating to pre-commit. Work in progress. (creates conflicts after formatting).

--- development_tools/git_hooks/pre-commit.sh
+++ development_tools/git_hooks/pre-commit.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+MAX_LINE_LENGTH=132
+
+git stash -q --keep-index
+committed_files=`git diff --cached --name-only HEAD`
+
 echo "Checking .c and .h file format ..."
-for file in `git diff-index --cached --name-only HEAD | grep -iE '\.(c|h)$' ` ; do
-      echo "${file}"
-      out=$(git clang-format -v --diff "${file}")
-      if [ "$out" != *"no modified files to format"* ] && [ "$out" != *"clang-format did not modify any files"* ]; then
-        if [ "$(git diff ${file})" == "" ]; then
-          echo "Automatically fixing formatting errors in ${file} with \"clang-format\"."
-          clang-format -i ${file}
-          git add ${file}
-        else
-          echo "File to be committed \"${file}\" has formatting errors that can be fixed automatically;"
-          echo "however, there have been changes since it was added, so we will abort commit to avoid conflicts."
-        exit 1;
-        fi
-      fi
+commited_c_files=`echo ${committed_files} | tr ' ' '\n ' | grep -iE '\.(c|h)$' `
+for file in $commited_c_files ; do
+  echo "${file}"
+  out=$(git clang-format -v --diff "${file}")
+  if [ "$out" != *"no modified files to format"* ] && [ "$out" != *"clang-format did not modify any files"* ]; then
+    echo "Automatically fixing formatting errors in ${file} with \"clang-format\"."
+    clang-format -i ${file}
+    git add ${file}
+  fi
 done
+
+echo "Checking .py and .pyi file format ..."
+commited_py_files=`echo ${committed_files} | tr ' ' '\n ' | grep -iE '\.py(|i)$' `
+echo $
+for file in $commited_py_files ; do
+  echo "${file}"
+  black -l $MAX_LINE_LENGTH  -q --check ${file}
+  if [ $? == 1 ]; then
+    echo "Automatically fixing formatting errors in ${file} with \"black\"."
+    black -l $MAX_LINE_LENGTH -q ${file}
+    git add ${file}
+  fi
+  git add ${file}
+done
+git stash pop -q
