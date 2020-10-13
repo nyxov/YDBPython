3ee966adcbe4ccd38f0921a28d4de0db12e134ad
Peter Goss <gossrock@gmail.com>
2020-10-13 00:23:07 +0000

    changed pre-commit script to use formating style config files and restore unmodified but automatically formated files after precommit script completes.

--- development_tools/git_hooks/pre-commit.sh
+++ development_tools/git_hooks/pre-commit.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-MAX_LINE_LENGTH=132
+committed_files=$(git diff --cached --name-only HEAD)
+changed_files=$(git diff --name-only | tr "\n" " ")
+restore_files=""
 
 git stash -q --keep-index
-committed_files=`git diff --cached --name-only HEAD`
 
-echo "Checking .c and .h file format ..."
-commited_c_files=`echo ${committed_files} | tr ' ' '\n ' | grep -iE '\.(c|h)$' `
-for file in $commited_c_files ; do
-  echo "${file}"
-  out=$(git clang-format -v --diff "${file}")
-  if [ "$out" != *"no modified files to format"* ] && [ "$out" != *"clang-format did not modify any files"* ]; then
-    echo "Automatically fixing formatting errors in ${file} with \"clang-format\"."
-    clang-format -i ${file}
+echo "Checking code format ..."
+for file in $committed_files ; do
+  formatted=false
+  if [[ "$file" == *".c" ]] || [[ "$file" == *".h" ]]; then
+    if ! clang-format --dry-run --Werror -style=file "$file" &>/dev/null; then
+      echo "    Automatically fixing formatting errors in $file with \"clang-format\"."
+      clang-format -i -style=file "$file"
+      formatted=true
+    fi
+  elif [[ "$file" == *".py" ]] || [[ "$file" == *".pyi" ]]; then
+    if ! black -q --check "$file"; then
+      echo "    Automatically fixing formatting errors in $file with \"black\"."
+      black -q "$file"
+      formatted=true
+    fi
+  fi
+  if $formatted; then
     git add ${file}
+    if ! [[ $changed_files =~ ( |^)$file( |$) ]]; then
+      restore_files="$restore_files $file"
+    fi
   fi
 done
 
-echo "Checking .py and .pyi file format ..."
-commited_py_files=`echo ${committed_files} | tr ' ' '\n ' | grep -iE '\.py(|i)$' `
-echo $
-for file in $commited_py_files ; do
-  echo "${file}"
-  black -l $MAX_LINE_LENGTH  -q --check ${file}
-  if [ $? == 1 ]; then
-    echo "Automatically fixing formatting errors in ${file} with \"black\"."
-    black -l $MAX_LINE_LENGTH -q ${file}
-    git add ${file}
-  fi
-done
 git stash pop -q
+if [[ "${restore_files}" != "" ]]; then
+  git restore ${restore_files}
+fi
