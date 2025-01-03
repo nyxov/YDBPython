eb0237acada80f0af903e4253ac67a1f1f482afa
Jon Badiali <jon@yottadb.com>
2025-01-03 17:53:15 -0500

    Updated `find-llvm-tool.sh` with latest fixes from other repos
    Background
    ----------
    
    Previously, running `clang-tidy-all.sh` would result in errors like the following:
    
    ```
    PATH/YDBPython/tools/ci/find-llvm-tool.sh: line 49: [: version: integer expression expected
    ```
    
    Fix
    ---
    
    This issue is fixed by:
    1. Incorporating changes to this file from other repos that account for version differences between tools
    2. Updating the line that sets `majorver` for `clang-tidy` to behave like the similar line for `clang-format` by revising the `awk` call there

--- tools/ci/find-llvm-tool.sh
+++ tools/ci/find-llvm-tool.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 #################################################################
 #								#
-# Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -10,43 +10,66 @@
 #	the license, please stop and do not read further.	#
 #								#
 #################################################################
+# This file is copied from YDBOcto/tools/ci/find-llvm-tool.sh
+#
 # Find an LLVM tool in the environment
 #
-# Takes no arguments and outputs the name of the executable.
+# Takes 2 arguments ("name" and "minimum version" of tool) and outputs the name of the executable.
 # If a recent enough version was not found, outputs nothing.
-
-set -e # Fail script if any command fails
-set -u # Enable detection of uninitialized variables
-set -o pipefail	# this way $? is set to zero only if ALL commands in a pipeline succeed. Else only last command determines $?
+set -eu
 
 tool="$1"
 version="$2"
 FOUND=
 
 exists() {
-	[ -x "$(which $1)" ]
+	[ -x "$(command -v "$1")" ]
 }
 
 # Ubuntu likes to name the tools after the version
 # NOTE: should be updated when later versions of LLVM are released
-for version in $(seq 11 -1 $version); do
-	if exists $tool-$version; then
-		FOUND=$tool-$version
-		break
+for version in $(seq 12 -1 "$version"); do
+	if exists "$tool-$version"; then
+		FOUND="$tool-$version"
+		# We found a recent enough version so return success
+		echo "$FOUND"
+		exit 0
 	fi
 done
 
-# No version suffix, we get what we get.
-if [ "" = "$FOUND" ]; then
-	# We didn't find it at all.
-	if ! exists $tool; then
-		exit 1
-	fi
-	FOUND=$tool
+# We didn't find a version specific tool. Check if tool without a specific version exists.
+if ! exists "$tool"; then
+	# We did not find generic tool. Exit with error.
+	exit 1
 fi
 
+# We found generic tool. Check if its version is at least the minimum we want.
+FOUND=$tool
+
 # Make sure we have a recent enough version.
-if [ "$($FOUND --version | grep version | awk '{print $3}' | cut -d '.' -f 1)" -ge $version ]; then
+# "clang-format --version" output is different depending on the versions.
+# Example outputs on various linux distributions
+#	RHEL 8       : clang-format version 11.0.0 (Red Hat 11.0.0-1.module+el8.4.0+8598+a071fcd5)
+#	Ubuntu 20.04 : clang-format version 10.0.0-4ubuntu1
+#	Arch Linux   : clang-format version 12.0.1
+#	Ubuntu 21.04 : Ubuntu clang-format version 12.0.0-3ubuntu1~21.04.1
+# We want to extract the major version from the above (11, 10, 12, 12 respectively). Hence the gsub() function usage below.
+#
+# "clang-tidy --version" output is different too.
+# LLVM (http://llvm.org/):
+#  LLVM version 10.0.0
+#
+#  Optimized build.
+#  Default target: x86_64-pc-linux-gnu
+#  Host CPU: haswell
+
+if [ "$tool" = "clang-tidy" ]; then
+	majorver=$($FOUND --version | awk '/version/ {gsub(".*LLVM version", ""); print $1}' | cut -d '.' -f 1)
+elif [ "$tool" = "clang-format" ]; then
+	majorver=$($FOUND --version | awk '/version/ {gsub(".*clang-format version", ""); print $1}' | cut -d '.' -f 1)
+fi
+
+if [ "$majorver" -ge "$version" ]; then
 	echo "$FOUND"
 else
 	exit 1
